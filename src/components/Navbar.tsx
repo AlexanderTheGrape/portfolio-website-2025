1bfe8db74f82698ec03e8d474acb367e211af894
amonk <amonk446@gmail.com>
2025-04-09 07:40:05 +1000

    production build

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { motion } from 'framer-motion';
+//import { motion } from 'framer-motion';
 import { Button } from '@/components/ui/button';
 
 const Navbar = () => {
