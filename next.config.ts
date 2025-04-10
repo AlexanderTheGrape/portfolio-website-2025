b6b8f4ca703822bee041a770994fcb0c5713381f
amonk <amonk446@gmail.com>
2025-04-10 21:26:02 +1000

    made webpage static

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // Disable server-side features
+  experimental: {
+    appDir: true,
+  },
   // devIndicators: false
 };
 
